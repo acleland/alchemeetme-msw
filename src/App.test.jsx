5c2f72878ab98f095346c808713347fefc92a485
Andrea Cleland <andreaclelan@gmail.com>
2022-04-27 15:10:08 -0700

    added server.use() thanks to Dan's help

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -14,7 +14,7 @@ const user = {
   id: 1,
   created_at: '2021-12-13T00:17:29+00:00',
   // 🚨 Add a name here
-  name: 'Sasuke 🌬️🔥',
+  name: 'Whatever',
   avatar: 'https://thumbs.gfycat.com/NiceRequiredGrunion-size_restricted.gif',
   header: 'https://static.wikia.nocookie.net/naruto/images/5/50/Team_Kakashi.png',
   likes: ['React', 'Anime', 'Traveling', 'Living', 'Tower Defense Games', 'Card Games'],
@@ -59,6 +59,11 @@ test('Should render the header with Sasuke 🌬️🔥', async () => {
     motto: 'Res Non Verba',
     color: 'crimson',
   }
+  server.use(
+    rest.get(`${process.env.REACT_APP_SUPABASE_URL}/rest/v1/users`, (req, res, ctx) =>
+      res(ctx.json([sasuke]))
+    )
+  )
 
   // 🚨 Use the server to change the response for this test
 
